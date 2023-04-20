dc8496726f665bc284ff46f0722f5df5014913a7
“Martin <marticarrizo1995@gmail.com>
2023-04-20 07:30:09 +0200

    ajuste computer

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -21,9 +21,9 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -4, -2.2] : [0, -3.45,0 ]}
-        rotation={[-0.01, -0.2, -0.1]}
+        scale={isMobile ? 0.56 : 0.75}
+        position={isMobile ? [0, -3, -0.2] : [0, -3.45,0 ]}
+        rotation={isMobile?[0,0,0]:[-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
@@ -33,21 +33,12 @@ const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
-    // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 500px)");
-
-    // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
-
-    // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
       setIsMobile(event.matches);
     };
-
-    // Add the callback function as a listener for changes to the media query
     mediaQuery.addEventListener("change", handleMediaQueryChange);
-
-    // Remove the listener when the component is unmounted
     return () => {
       mediaQuery.removeEventListener("change", handleMediaQueryChange);
     };
